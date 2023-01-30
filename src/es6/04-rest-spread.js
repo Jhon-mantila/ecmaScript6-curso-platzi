a66b840e38a478ec5de689b6cc99af65945c1cee
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-01-29 19:48:33 -0500

    spread operator (...) permite agrugar los valores ya sea en un objeto o un array

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -13,3 +13,23 @@ let user = {
 let {username, age } = user;
 console.log(username, user.age);
 
+//spread operator
+let person = {name: 'oscar', age: 28};
+let country = 'CO';
+//se le aplica la desestructuración al objeto y se le agrega la variable country
+let data = { person, country};
+console.log(data)
+//los tres puntos me pertimen que sean del mismo objeto sin los puntos crearia un objeto nuevo donde englobaria dos objetos
+data = { ...person, country};
+console.log(data);
+data = { id: 1, ...person, country};
+console.log(data);
+
+//rest
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+
+    return num + values[0];
+}
+sum(1, 5,4,3,2);
