aa9a4c4c9cc13284ad8189f9ffb1fee46a0a7bf9
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-02-04 19:55:11 -0500

    Object keys, entries y values es8

---
+++ src/es8/02-object-keys.js
@@ -0,0 +1,8 @@
+const countries = {
+    MX : 'Mexico', 
+    CO : 'Colombia',
+    CL : 'Chile',
+    PE : 'Perú'
+   };
+
+   console.log(Object.keys(countries)); // crea un array con las llaves del objecto
