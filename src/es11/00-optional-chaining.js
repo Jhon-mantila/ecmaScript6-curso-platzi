d99e43f29ba7d8b32994383b2ccd0e5912f8b611
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-02-05 16:28:30 -0500

    optional chainig / encadenamiento opcional

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,14 @@
+//Encadenamiento opcional. optional chainigng ?
+
+const users = {
+    jhon:{
+        country: 'MX'
+    },
+    jessika:{
+        country: 'CO'
+    }
+}
+
+console.log(users.jhon.country);
+console.log(users.jhon.age);
+console.log(users?.daniela?.country);
