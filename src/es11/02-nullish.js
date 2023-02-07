5eccae7b197eb827236e934df4729352e5063793
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-02-06 19:33:28 -0500

    nullish

---
+++ src/es11/02-nullish.js
@@ -0,0 +1,5 @@
+// dos veces el signo de pregunta ?? te permite validar si el operador es nullo y si es nullo agsina el que le damos por
+const anotherNumber = null;
+const validate = anotherNumber ?? 5;
+
+console.log(validate);
