33bc098bf9039d7fcd1f8c09cc2a1826f9d5c143
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-02-08 20:41:30 -0500

    Ejercicio con promesas y asyn y await y import dynamic

--- src/es11/06-dynamic-import.js
+++ src/es11/06-dynamic-import.js
@@ -1,7 +1,33 @@
 const button = document.getElementById("btn");
+const button2 = document.getElementById("btn2");
 
 button.addEventListener('click', async function() {
     const module = await import('./module.js');
     console.log(module);
     module.hello();
 });
+
+/*Ejemplo con proesa */
+const ruta = "./module.js";
+// Utilizando promesas
+import(ruta)
+    .then( modulo => {
+        modulo.miFuncion1();
+        modulo.miFuncion2();
+    })
+    .catch(error => console.log(error));
+
+// Utilizando async/await
+async function importarModulo(rutaDelModulo) {
+    const modulo = await import(rutaDelModulo)
+    modulo.miFuncion1();
+    modulo.miFuncion2();
+}
+
+importarModulo(ruta);
+
+button2.addEventListener("click", async function () {
+    const modulo = await import('./module.js')
+    modulo.miFuncion1();
+    modulo.miFuncion2();
+})
--- src/es11/module.js
+++ src/es11/module.js
@@ -1,3 +1,11 @@
 export function hello () {
     console.log("Hello world!!!");
 }
+
+export function miFuncion1(){
+    console.log("Mi primera función");
+}
+
+export function miFuncion2(){
+    console.log("Mi segunda función");
+}
