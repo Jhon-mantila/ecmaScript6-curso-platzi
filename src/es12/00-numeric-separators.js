b0215cd2ed0661b0d70e6501e6db025b2c214779
Jhon-mantila <kaiser_4tu@hotmail.com>
2023-02-08 21:19:30 -0500

    separador para numeros guion piso y replace

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,5 @@
+//con raya al piso se puede separar los ceros para idenficiar mejor el número. no afecta el resultado
+const value = 100_000_000_000_000;
+
+console.log(value);
+
---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,5 @@
+const txt = "JavaScript es un maravilloso legunaje de programación, gracias..";
+
+const replaceString = txt.replace("JavaScript", "Java");
+
+console.log(replaceString);
